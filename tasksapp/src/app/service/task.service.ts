af176d13a3a5d68f8ce9d5aa14089302e2c69acf
vhtor <victoreverend@gmail.com>
2022-02-17 18:21:32 -0300

    Added methods for list, add, get, filter by status, filter by priority and delete tasks

--- tasksapp/src/app/service/task.service.ts
+++ tasksapp/src/app/service/task.service.ts
@@ -1,7 +1,9 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, throwError } from 'rxjs';
 import { tap, catchError } from 'rxjs/operators';
+import { Priority } from '../enum/priority.enum';
+import { Status } from '../enum/status.enum';
 import { CustomResponse } from '../interface/custom-response';
 
 @Injectable({ providedIn: 'root' })
@@ -31,6 +33,50 @@ export class TaskService {
     catchError(this.handleError)
   );
 
+  filterByStatus$ = (status: Status, response: CustomResponse) => <Observable<CustomResponse>>
+  new Observable<CustomResponse>(
+    subscriber => {
+      console.log(response);
+      subscriber.next(
+        status === Status.ALL ? 
+          { ...response, message: `Tasks filtradas pelo status: ${status}` } :
+          {
+            ...response,
+            message: response.data.tasks.filter(task => task.status === status).length > 0 ?
+            `Tasks filtradas pelo status: ${status}` : `Não há tasks com o status: ${status}`,
+            data: { tasks: response.data.tasks.filter(task => task.status === status) }
+          }
+      );
+      subscriber.complete();
+    }
+  )
+  .pipe(
+    tap(console.log),
+    catchError(this.handleError)
+  );
+
+  filterByPriority$ = (priority: Priority, response: CustomResponse) => <Observable<CustomResponse>>
+  new Observable<CustomResponse>(
+    subscriber => {
+      console.log(response);
+      subscriber.next(
+        priority === Priority.ALL ? 
+          { ...response, message: `Tasks filtradas pela prioridade: ${priority}` } :
+          {
+            ...response,
+            message: response.data.tasks.filter(task => task.priority === priority).length > 0 ?
+            `Tasks filtradas pela prioridade: ${priority}` : `Não há tasks com a prioridade: ${priority}`,
+            data: { tasks: response.data.tasks.filter(task => task.priority === priority) }
+          }
+      );
+      subscriber.complete();
+    }
+  )
+  .pipe(
+    tap(console.log),
+    catchError(this.handleError)
+  );
+
   delete$ = (taskId: number) => <Observable<CustomResponse>>
   this.http.delete<CustomResponse>(`${this.apiUrl}/task/delete/${taskId}`)
   .pipe(
@@ -38,7 +84,8 @@ export class TaskService {
     catchError(this.handleError)
   );
 
-  handleError(handleError: any): Observable<never> {
-    return throwError ('Method not implemented.');
+  private handleError(error: HttpErrorResponse): Observable<never> {
+    console.log(error);
+    return throwError(`Um erro ocorreu - Código do erro: ${error.status}`);
   }
 }
